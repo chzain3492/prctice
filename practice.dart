27b5c9c52e6cebf4c3bfab544850637f9e4c278e
M Zain Yasin <zainyasin349292@gmail.com>
2025-05-19 09:53:14 -0700

    ractice 1.0

---
+++ practice.dart
@@ -0,0 +1,17 @@
+void main() {
+  int age = 20;                    
+  double height = 5.9;            
+  String name = "Ali";            
+  bool isStudent = true;        
+
+  var country = "Pakistan";      
+  dynamic x = 10;                
+  x = "Hello";                   
+  print(x);
+
+  print(name);                   
+  print(age);
+  print(height);
+  print(isStudent);
+  print(country);
+// }
