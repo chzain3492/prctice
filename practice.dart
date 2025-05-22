dbbc9f414b90186cc2f6b1f8b3afe688250c2803
M Zain Yasin <zainyasin349292@gmail.com>
2025-05-22 07:55:52 -0700

    simple variable exm

--- practice.dart
+++ practice.dart
@@ -1,3 +1,14 @@
-// string name: zain;
-string? name;
-// print("name=$name");//simple example of null
+void main() {
+  int age = 25;
+  print(age);
+  String name = "Ali";
+  print("My name is $name");
+  int score = 10;
+  score = 15;
+  print("Updated score: $score");
+  int a = 5;
+  int b = 7;
+  int sum = a + b;
+  print("Sum is $sum");
+}
+// 
