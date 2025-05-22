18358abe9a5f46c1828937f656dbc7447e8e5541
M Zain Yasin <zainyasin349292@gmail.com>
2025-05-22 07:44:16 -0700

    practice

--- practice.dart
+++ practice.dart
@@ -1,17 +1,1 @@
-void main() {
-  int age = 20;                    
-  double height = 5.9;            
-  String name = "Ali";            
-  bool isStudent = true;        
-
-  var country = "Pakistan";      
-  dynamic x = 10;                
-  x = "Hello";                   
-  print(x);
-
-  print(name);                   
-  print(age);
-  print(height);
-  print(isStudent);
-  print(country);
-// }
+// string name: zain;
